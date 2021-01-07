f63dfff8ae1b75f3a9e976efaa455a3c7e8c618b
Jordan Beck <jordanfbeck@gmail.com>
2021-01-07 11:00:05 -0700

    last second

--- lib/museum.rb
+++ lib/museum.rb
@@ -44,6 +44,8 @@ class Museum
   end
 
   def draw_lottery_winner(exhibit)
-    ticket_lottery_contestants(exhibit).sample.name
+    unless ticket_lottery_contestants(exhibit) == nil
+      ticket_lottery_contestants(exhibit).sample.name
+    end
   end
 end
